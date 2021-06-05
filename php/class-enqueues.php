bc9cc5e7b507d1671cdc4c082c48e169fba34c24
Mattia Zoccarato <mattia.zoccarato@gmail.com>
2021-06-05 16:11:29 +0200

    update home, category, product pag snippet

--- php/class-enqueues.php
+++ php/class-enqueues.php
@@ -92,9 +92,6 @@
 		 */
 		public function run() {
 			// Enqueue Front-end JS.
-			
-			//exit;
-			
 			add_action( 'wp_enqueue_scripts', array( $this, 'public_enqueue_scripts' ), 10 );
 		}
 		
@@ -117,52 +114,223 @@
 					$this->plugin_slug . '-public-js',
 					$public_js_url,
 					array( 'jquery' ),
-					filemtime( $public_js_path ),
-					true
+					false,
+					false
 				);
 				
+				$this->script = $this->getHeaders();
 				
-				$header_request = getallheaders();
-				$header_response = headers_list();
-				$header_response_status_code = http_response_code();
-				
-				
-				$header_response_filtered = array();
-				
-				
-				
-				foreach ($header_response as $response) {
-					$explode_pos = strpos($response,':');
-					$key = substr($response, 0, $explode_pos);
-					if($key !== 'Link'){
-						$value = substr($response, $explode_pos);
-						$header_response_filtered[] = array($key, $value);
-					}
+				if(is_front_page() || is_home()) {
+					$this->appendHomeJS();
 				}
-				
-				$headers = array(
-					'request'=>array(
-						'Accept-Encoding'=>$header_request['Accept-Encoding'],
-						'Accept-Language'=>$header_request['Accept-Language'],
-						'Cookie'=>$header_request['Cookie']
-					),
-					'response'=>array(
-						$header_response_filtered,
-						'status_code'=>$header_response_status_code
-					)
-				);
-				
-				$script = '
-	 		 		var sirioCustomObject = {};
-	 				sirioCustomObject.headers = '.json_encode($headers).';
-	 		 ';
+				else if(is_product()) {
+					$this->appendProductJS();
+				}
+				else if(is_product_category()) {
+					$this->appendProductCategoryJS();
+				}
+				else if (is_search()) {
+					$this->appendProductSearchJS();
+				}
+				else if (is_checkout()) {
+					$this->appendCheckoutJS();
+				}
+				else if (is_order_received_page()) {
+					$this->appendCheckoutSuccessJS();
+				}
 				
 				wp_add_inline_script(
 					$this->plugin_slug . '-public-js',
-					$script,
+					$this->script,
 					'after'
 				);
 			}
 			
 		}
+		
+		protected function getHeaders(){
+			$header_request = getallheaders();
+			$header_response = headers_list();
+			$header_response_status_code = http_response_code();
+			
+			$header_response_filtered = array();
+			
+			foreach ($header_response as $response) {
+				$explode_pos = strpos($response,':');
+				$key = substr($response, 0, $explode_pos);
+				if($key !== 'Link'){
+					$value = substr($response, $explode_pos);
+					$header_response_filtered[] = array($key, $value);
+				}
+			}
+			
+			$headers = array(
+				'request'=>array(
+					'Accept-Encoding'=>$header_request['Accept-Encoding'],
+					'Accept-Language'=>$header_request['Accept-Language'],
+					'Cookie'=>$header_request['Cookie']
+				),
+				'response'=>array(
+					$header_response_filtered,
+					'status_code'=>$header_response_status_code
+				)
+			);
+			
+			$script = 'var sirioCustomObject = {};
+	 				 sirioCustomObject.headers = '.json_encode($headers).';';
+			
+			return $script;
+		}
+		
+		private function appendHomeJS() {
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$currency_code = get_woocommerce_currency();
+			$this->script = ' //<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.pageType = "home";
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+		}
+		
+		private function appendProductJS() {
+			$current_product = wc_get_product();
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$currency_code = get_woocommerce_currency();
+			$image_id  = $current_product->get_image_id();
+			$image_url = wp_get_attachment_image_url( $image_id, 'full' );
+			
+			if($current_product->get_variation_price( 'max' )){
+				// Regular price min and max
+				$regular_price = $current_product->get_variation_regular_price( 'max' );
+				// Sale price min and max
+				$sale_price = $current_product->get_variation_sale_price( 'max' );
+			}
+			else{
+				$regular_price = $current_product->regular_price;
+				$sale_price = $current_product->sale_price;
+			}
+			
+			
+			$this->script = ' //<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.productDetails = {"sku":"'.$current_product->get_slug().'", "name":"'.$current_product->get_name().'","image":"'.$image_url.'","description":"'.addslashes(str_replace("\n","", str_replace("\r","", str_replace("\t","", strip_tags($current_product->get_description()))))).'","price":"'.$regular_price.'","special_price":"'.$sale_price.'"};
+                     sirioCustomObject.pageType = "product";
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+			
+		}
+		
+		private function appendProductCategoryJS() {
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$page = (get_query_var('paged')) ? get_query_var('paged') : 1;;
+			$current_category = get_queried_object();
+			
+			$products = wc_get_products(array(
+				'category' => array($current_category->slug),
+			));
+			$max_product_count = count($products);
+			$limit = get_option( 'posts_per_page' );
+			$products_count = $limit;
+			$currency_code = get_woocommerce_currency();
+			
+			if($max_product_count % $limit > 0){
+				$pages = (int)($max_product_count / $limit) + 1 ;
+			}
+			else{
+				$pages = $max_product_count / $limit ;
+			}
+			if($page == $pages){
+				$products_count = $max_product_count % $limit;
+			}
+			
+			$thumbnail_id = get_term_meta( $current_category->term_id, 'thumbnail_id', true );
+			$image_url = wp_get_attachment_url( $thumbnail_id );
+			
+			$this->script = ' //<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.categoryDetails = {"name":"'.$current_category->name.'","image":"'.$image_url.'","description":"'.addslashes(str_replace("\n","", str_replace("\r","", str_replace("\t","",strip_tags($current_category->description))))).'"};
+                     sirioCustomObject.pageType = "category";
+                     sirioCustomObject.numProducts = '.$products_count.';
+                     sirioCustomObject.pages = '.$pages.';
+                     sirioCustomObject.currentPage = '.$page.';
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+		}
+		
+		private function appendProductSearchJS() {
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$page = (get_query_var('paged')) ? get_query_var('paged') : 1;;
+			$current_category = get_queried_object();
+			$max_product_count = count($products);
+			$limit = get_option( 'posts_per_page' );
+			$products_count = $limit;
+			
+			$currency_code = get_woocommerce_currency();
+			$query = new ProductSearchQuery();
+			$search_string = Tools::getValue('s');
+			$query->setSortOrder(new SortOrder('product', 'position', 'desc'))
+				->setSearchString($search_string);
+			$provider = $this->getProductSearchProviderFromModules($query);
+			
+			// if no module wants to do the query, then the core feature is used
+			if (null === $provider) {
+				$provider = $this->getDefaultProductSearchProvider();
+			}
+			// the search provider will need a context (language, shop...) to do its job
+			$context = $this->getProductSearchContext();
+			$result = $provider->runQuery(
+				$context,
+				$query
+			);
+			$max_product_count = $result->getTotalProductsCount();
+			if($max_product_count % $limit > 0){
+				$pages = (int)($max_product_count / $limit) + 1 ;
+			}
+			else{
+				$pages = $max_product_count / $limit ;
+			}
+			if($page == $pages){
+				$products_count = $max_product_count % $limit;
+			}
+			$this->script = '//<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.pageType = "search";
+                     sirioCustomObject.numProducts = '.$products_count.';
+                     sirioCustomObject.pages = '.$pages.';
+                     sirioCustomObject.currentPage = '.$page.';
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+		}
+		
+		private function appendCheckoutJS() {
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$currency_code = get_woocommerce_currency();
+			$this->script = '//<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.pageType = "checkout";
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+		}
+		
+		private function appendCheckoutSuccessJS() {
+			$locale = explode('-', get_bloginfo('language'))[0];
+			$currency_code = get_woocommerce_currency();
+			
+			if(isset($_COOKIE['cart_new'])){
+				unset($_COOKIE['cart_new']);
+			}
+			
+			$this->script = '//<![CDATA[
+                     '.$this->script.'
+                     sirioCustomObject.pageType = "checkout_success";
+                     sirioCustomObject.locale = "'.$locale.'";
+                     sirioCustomObject.currency = "'.$currency_code.'";
+                     //]]>';
+		}
+		
 	}
